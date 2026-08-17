97569272364a7e902db8cb2698405f6e07f4a677
ahmed <aahmed50@gmail.com>
2026-08-17 22:00:12 +0300

    create ui for tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:my_movies_app/Ui/home/home_screen.dart';
+import 'package:my_movies_app/utils/app_route.dart';
 import 'package:my_movies_app/utils/app_theme.dart';
 
 import 'l10n/app_localizations.dart';

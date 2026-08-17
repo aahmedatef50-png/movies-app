912f0701421690839835a284f0f658409fdff137
Osamaaaothman <osamaammarothman@gmail.com>
2026-08-17 20:49:09 +0300

    ui wedgit

---
+++ lib/model/app_model.dart
@@ -0,0 +1,15 @@
+import '../utils/app_image.dart';
+
+class AppModel {
+  static List<String> avatars = [
+    AppImage.avatar1,
+    AppImage.avatar2,
+    AppImage.avatar3,
+    AppImage.avatar4,
+    AppImage.avatar5,
+    AppImage.avatar6,
+    AppImage.avatar7,
+    AppImage.avatar8,
+    AppImage.avatar9,
+  ];
+}

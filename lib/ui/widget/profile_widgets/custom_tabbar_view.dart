72587a27622a7f2797d132e1610cfabdb1763f5e
MariamWael411 <dfgsadffg344@gmail.com>
2026-08-21 22:20:13 +0300

    add api folder

--- lib/ui/widget/profile_widgets/custom_tabbar_view.dart
+++ lib/ui/widget/profile_widgets/custom_tabbar_view.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:my_movies_app/utils/app_image.dart';
 
+import '../../../utils/app_config.dart';
+
 class CustomTabBarView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = AppConfig.width(context);
+    var height = AppConfig.height(context);
     return TabBarView(
 
       children: [
-      Center(child: Image(image: AssetImage(AppImage.popCorn),)),
-      Center(child: Image(image: AssetImage(AppImage.popCorn),)),
+        Center(child: Image(image: AssetImage(AppImage.popCorn),
+          width: width * 0.4,
+          height: height * 0.16,
+          fit: BoxFit.fill,
+        )),
+        Center(child: Image(image: AssetImage(AppImage.popCorn),
+          width: width * 0.4,
+          height: height * 0.16,
+          fit: BoxFit.fill,)),
     ],
     );
   }

795b20f033dea78605905b58fcea838a354b163d
Osamaaaothman <osamaammarothman@gmail.com>
2026-08-28 21:57:27 +0300

    edit: Movie Grid

--- lib/Ui/widget/movies_grid/movies_grid.dart
+++ lib/Ui/widget/movies_grid/movies_grid.dart
@@ -3,14 +3,15 @@ import 'package:my_movies_app/Ui/widget/movies_grid/movie_card.dart';
 import 'package:my_movies_app/api/model/movies/movies.dart';
 
 class MoviesGrid extends StatelessWidget {
-  const MoviesGrid({required this.movies});
+  const MoviesGrid({required this.movies, this.cols=3});
   final List<Movies> movies;
+  final int cols;
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
       padding: EdgeInsets.symmetric(vertical: 24,horizontal: 16),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
+            crossAxisCount: cols,
             childAspectRatio: 2/3,
           crossAxisSpacing: 16,
           mainAxisSpacing: 16

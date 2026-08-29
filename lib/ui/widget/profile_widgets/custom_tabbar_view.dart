bc01ff82f729fb07108ce7a0ecb798bd64312b13
Osamaaaothman <osamaammarothman@gmail.com>
2026-08-29 11:28:04 +0300

    fix all errors

--- lib/ui/widget/profile_widgets/custom_tabbar_view.dart
+++ lib/ui/widget/profile_widgets/custom_tabbar_view.dart
@@ -1,52 +1,59 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:my_movies_app/Ui/widget/movies_grid/movies_grid.dart';
 import 'package:my_movies_app/api/model/movies/movies.dart';
 import 'package:my_movies_app/model/favourite.dart';
+import 'package:my_movies_app/utils/app_config.dart';
 import 'package:my_movies_app/utils/app_image.dart';
-import 'package:my_movies_app/utils/firebase_osama_test.dart';
 import 'package:my_movies_app/utils/firebase_utils.dart';
 
-import '../../../utils/app_config.dart';
-
-class CustomTabBarView extends StatefulWidget {
-  @override
-  State<CustomTabBarView> createState() => _CustomTabBarViewState();
-}
-
-class _CustomTabBarViewState extends State<CustomTabBarView> {
-  List<Movies> movies=[];
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    getAllFaviorates();
-  }
-  void getAllFaviorates()async {
-   var querySnapShot= await FirebaseUtils.getFavouritesCollection().get();
-   movies=querySnapShot.docs.map((doc){
-    return Movies(url: doc.data().url,id: doc.data().movieId,rating: doc.data().rating);
-   }).toList();
-   print(movies);
-setState(() {
-
-});
-  }
-
+class CustomTabBarView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = AppConfig.width(context);
     var height = AppConfig.height(context);
-    return TabBarView(
 
+    return TabBarView(
       children: [
-        Center(child:
-        MoviesGrid(movies: movies,)
+        Center(
+          child: StreamBuilder<QuerySnapshot<Favourite>>(
+            stream: FirebaseUtils.getFavouritesCollection().snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              }
+              if (snapshot.hasError) {
+                return Text('Something went wrong');
+              }
+              final docs = snapshot.data?.docs ?? [];
+              final movies = docs.map((doc) {
+                final fav = doc.data();
+                return Movies(
+                  url: fav.url,
+                  id: fav.movieId,
+                  rating: fav.rating,
+                  largeCoverImage: fav.url
+                  // include coverImage here too, per the earlier fix
+                );
+              }).toList();
+
+              if (movies.isEmpty) {
+                return const Text('No favourites yet');
+              }
+              return MoviesGrid(movies: movies);
+            },
+          ),
         ),
-        Center(child: Image(image: AssetImage(AppImage.popCorn),
-          width: width * 0.4,
-          height: height * 0.16,
-          fit: BoxFit.fill,)),
-    ],
+        Center(
+          child: Image(
+            image: AssetImage(AppImage.popCorn),
+            width: width * 0.4,
+            height: height * 0.16,
+            fit: BoxFit.fill,
+          ),
+        ),
+      ],
     );
   }
 }
